8161838d5c1ea653f4a784d850c277a4f479e4d0
raihan ardianata <raihan.ardianata@gmail.com>
2022-10-11 06:23:10 +0700

    perbaikan import file

--- pages/example/image.js
+++ pages/example/image.js
@@ -1,12 +1,11 @@
 import { Grid, ImageList, ImageListItem } from "@mui/material";
-import BaseCard from "../src/components/baseCard/BaseCard";
+import BaseCard from "components/baseCard/BaseCard";
 
 function srcset(image, size, rows = 1, cols = 1) {
   return {
     src: `${image}?w=${size * cols}&h=${size * rows}&fit=crop&auto=format`,
-    srcSet: `${image}?w=${size * cols}&h=${
-      size * rows
-    }&fit=crop&auto=format&dpr=2 2x`,
+    srcSet: `${image}?w=${size * cols}&h=${size * rows
+      }&fit=crop&auto=format&dpr=2 2x`,
   };
 }
 
